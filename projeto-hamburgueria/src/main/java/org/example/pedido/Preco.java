f16cc7aba14048f50e6144bc8542ddb8f6a67228
vitorrosa0 <vrfrosa1@gmail.com>
2026-05-31 14:36:49 -0300

    classes de Preço

---
+++ projeto-hamburgueria/src/main/java/org/example/pedido/Preco.java
@@ -0,0 +1,9 @@
+package org.example.pedido;
+
+public class Preco {
+    public static final double HAMBURGUER_TRADICIONAL = 25.0;
+    public static final double HAMBURGUER_VEGANO = 22.0;
+    public static final double BEBIDA = 8.0;
+
+    private Preco() {}
+}
